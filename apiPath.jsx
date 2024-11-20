2b6e291f4088cc161dba7e66f4ecb48253beda00
srinu8688 <srinu868890@gmail.com>
2024-11-20 17:26:52 +0530

    changed the backend api url

--- apiPath.jsx
+++ apiPath.jsx
@@ -1,3 +1,3 @@
 //export const API_URL="http://localhost:5000"
 
-export const API_URL="https://backend-node-staffing.onrender.com/"
+export const API_URL="https://backend-node-staffing-1.onrender.com"
